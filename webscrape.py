a76438216f6dcceb24df64598720b93412aafcdc
Edie <ehze@live.com>
2017-09-15 18:40:04 -0700

    Delete webscrape.py

--- webscrape.py
+++
@@ -1,9 +0,0 @@
-# LET'S MAKE THE PERFECT BANANA BREAD
-
-# PART ONE
-# SCRAPE ALLRECIPES.COM
-# http://allrecipes.com/search/results/?wt=banana%20bread&sort=re
-
-# PART TWO
-# SCRAPE FOOD.COM
-# http://www.food.com/search/banana+bread?pn=2
