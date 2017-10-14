2a48e522b0b7152578f9bde361c5ce21ccd3a9cb
Edie <ehze@live.com>
2017-10-14 00:43:17 -0700

    some more functions

--- 02 cleaning/recipe_analysis.R
+++ 02 cleaning/recipe_analysis.R
@@ -1,6 +1,12 @@
+# LIBRARIES
+library(stringr)
+
 # LOAD IN DATA
 bbdata = read.csv("/Users/EDIE/Desktop/banana_bread_df.csv", as.is=TRUE)
 attach(bbdata)
+
+
+
 
 # RATINGS OVERALL
 hist(longRating, main="Histogram of Ratings")
@@ -19,6 +25,13 @@ hist(reviews, main="Histogram of Reviewers Reviewing Recipes")
 plot(reviews, made.it, ylim=c(0,1000), xlim=c(0,1000))
 plot(made.it, reviews, ylim=c(0,500), xlim=c(0,500))
 
+line = lm(made.it[reviews>250] ~ reviews[reviews>250])
+plot(reviews[reviews>250], made.it[reviews>250], ylim=c(0,1000), xlim=c(0,1000))
+abline(line)
+
+
+
+
 
 # ONE RECIPE
 bbdata$recipe[1]
@@ -26,31 +39,47 @@ ingredients = strsplit(bbdata$recipe[1], "\', ")[[1]]
 ingredients2 = unlist(lapply(ingredients, function(x) gsub("'","", x)))
 ingredients3 = unlist(lapply(ingredients2, function(x) gsub("\\[|\\]","", x)))
 
-# CUTTING UP RECIPES
-library(stringr)
-str_match(alice, ".*\\.D([0-9]+)\\.LIS.*")[, 2]
+split_into_ingredients = function(long_ass_string) {
+  ingredients = strsplit(long_ass_string, "\', ")[[1]]
+  ingredients2 = unlist(lapply(ingredients, function(x) gsub("'","", x)))
+  ingredients3 = unlist(lapply(ingredients2, function(x) gsub("\\[|\\]","", x)))
+  ingredients3
+}
 
+
+# NUMBER AMOUNTS
 ingredients3[1]
 grepl("[0-9]+ ([0-9]/[0-9] )?",ingredients3[1])
 str_match(ingredients3[1], "[0-9]+")
 str_match(ingredients3[2], "[0-9]+")
 str_extract(ingredients3[10], "[0-9]+ ([0-9]/[0-9])")
+
+# THIS IS THE BETTER REGEX
 str_extract(ingredients3[2], "[0-9](/[0-9]?)+( [0-9]/[0-9])?")
 str_extract(ingredients3[10], "[0-9](/[0-9]?)?+( [0-9]/[0-9])?")
 
-check_recipe = function(listed_ingredient) {
+check_recipe_number = function(listed_ingredient) {
   number = str_extract(listed_ingredient, "[0-9](/[0-9]?)?+( [0-9]/[0-9])?")
   return(number)
 }
 
-numbers = unlist(lapply(ingredients, check_recipe))
+numbers = unlist(lapply(ingredients, check_recipe_number))
 numbers
 
 # FUNCTION TO CONVERT FRACTIONS
 fraction_converter = function(num_string) {
   if (grepl("\\/", num_string)) {
-    num_denom = strsplit(num_string,"\\/")[[1]]
-    dec = as.integer(num_denom[1]) / as.integer(num_denom[2])
+    if (nchar(num_string)<4) {
+      num_denom = strsplit(num_string,"\\/")[[1]]
+      dec = as.integer(num_denom[1]) / as.integer(num_denom[2])
+    }
+    else {
+      mixed_no = strsplit(num_string, "\\/")[[1]]
+      whole_and_num = strsplit(mixed_no[1], " ")[[1]]
+      whole = as.integer(whole_and_num[1])
+      frac = as.integer(whole_and_num[2]) / as.integer(mixed_no[2])
+      dec = whole + frac
+    }
   }
   else {
     dec = as.integer(num_string)
@@ -58,8 +87,38 @@ fraction_converter = function(num_string) {
   dec
 }
 
-# FUNCTION TO CHECK FOR MIXED NUMBER CHECKING
-if (grepl("([0-9] )?[0-9]/[0-9]", numbers[10])) {
-  mixed_no = strsplit(numbers[10], " ")[[1]]
-  sum(unlist(lapply(mixed_no, fraction_converter)))
+converted_numbers = unlist(lapply(numbers, fraction_converter))
+
+
+
+# GET UNIT MEASUREMENTS
+# TAKE ingredients3
+# MAKE A FUNCTION THAT USES REGEX TO COLLECT
+# CUPS, TEASPOONS, TABLESPOONS, OUNCES, SERVING <-- COLLECT THAT INFORMATION
+
+# IF (grepl("pattern", "ingredient_line")) { str_extract the measurement }
+
+if (grepl("[0-9]+ ([A-z])+", ingredients3[3])) {
+  str_extract(ingredients3[3], "[A-z]+")
 }
+
+# CHECK THE MEASUREMENT
+check_measurement = function (v) {
+  if (grepl("[0-9]+ ([A-z])+", v)) {
+    if (grepl("cup(s)?|[A-z]+spoon(s)?", v)) {
+      str_extract(v, "[A-z]+")
+    } else {
+      0
+    }
+  }
+}
+
+unlist(lapply(ingredients3, check_measurement))
+
+
+
+# SPLIT ALL INGREDIENTS
+all_recipes = lapply(bbdata$recipe, split_into_ingredients)
+numbers = lapply(all_recipes, check_recipe_number)
+converted_numbers = lapply(numbers, fraction_converter)
+the_measurements = lapply(all_recipes, check_measurement)
